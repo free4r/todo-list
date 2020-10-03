0ee87effaee080aeda759f1f6db692b04e5e57fb
okycd <72317472+okycd@users.noreply.github.com>
2020-10-03 22:33:47 +0300

    Updated App config, improved path for ComponentScan and removed wrong config srub

--- src/main/java/com/company/todo/config/AppConfig.java
+++ src/main/java/com/company/todo/config/AppConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.company.todo")
+@ComponentScan(basePackages = "com.company.*")
 public class AppConfig extends WebMvcConfigurerAdapter {
 	@Bean
 	public ViewResolver viewResolver() {
@@ -22,10 +22,4 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
-
-	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
-	}
-
 }
